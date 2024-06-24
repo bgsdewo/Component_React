cf66b569919327457a647491b4d2184668f5c7d1
unknown <bgsdewoo20@gmail.com>
2024-06-24 20:59:20 +0700

    Cara Membuat Dan Menggunakan Component Di React.Js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello,React</h1>;
+  return (
+    <div>
+      <h1>Halo,React</h1>
+      <Food />
+    </div>
+  );
+}
+function Food() {
+  return (
+    <div>
+      <img src="food/bakso.jpg" alt="" />
+      <h2>Bakso Betawi</h2>
+      <p>Bakso Betawi khas mama lala</p>
+    </div>
+  );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
