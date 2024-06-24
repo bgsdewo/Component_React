9342c05c08ac8d434e98626f56ab040792894af9
unknown <bgsdewoo20@gmail.com>
2024-06-24 21:52:31 +0700

    Membuat Banyak Component Dan Mengaturnya

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//merupakan component utama
 function App() {
   return (
     <div>
-      <h1>Halo,React</h1>
-      <Food />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
+}
+
+function Header() {
+  return <h1>Warteg Padang</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Menu kita</h2>
+      <Food />
+      <Food />
+      <Food />
+    </div>
+
+    //ini gabisa
+    // <div>
+    //   <h2>Menu kita</h2>
+    //   <Food />
+    //   <Food />
+    //   <Food />
+    // </div>
+  );
+}
+function Footer() {
+  return <footer>{new Date().getFullYear()} Warteg Padang</footer>;
 }
 function Food() {
   return (
     <div>
-      <img src="food/bakso.jpg" alt="" />
+      <img src="food/bakso.jpg" alt="" width={100} height={70} />
       <h2>Bakso Betawi</h2>
       <p>Bakso Betawi khas mama lala</p>
     </div>
